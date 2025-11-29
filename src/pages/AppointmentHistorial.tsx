b460e0c8b1019b60d13f0dcbc553f2a2f6251235
Diego Pineda <zentoo31@gmail.com>
2025-11-28 19:03:55 -0500

    feat: create AppointmentHistorial component to display appointment history

---
+++ src/pages/AppointmentHistorial.tsx
@@ -0,0 +1,14 @@
+import AppointmentsList from "../components/AppointmentsList"
+
+function AppointmentHistorial() {
+  return (
+    <div className="bg-white rounded-2xl shadow-sm p-6">
+      <div className="space-y-6">
+        <AppointmentsList />
+
+      </div>
+    </div>
+  )
+}
+
+export default AppointmentHistorial
