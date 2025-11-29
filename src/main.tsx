750eeff1c075bdee4d50601a506b93ef49951940
Diego Pineda <zentoo31@gmail.com>
2025-11-28 19:03:32 -0500

    fix: update import path for Landing component to use pages directory

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './components/Landing.tsx';
+import Landing from './pages/Landing.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
