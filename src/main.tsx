9795f742eb58525ac770bd4370dd3c658faf3f50
zentoo31 <zentoo31@gmail.com>
2025-11-30 13:06:06 -0500

    refactor: wrap application in HeroUIProvider for improved context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import "../tailwind.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing.tsx';
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/dashboard' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <HeroUIProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/dashboard' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </HeroUIProvider>
   </StrictMode >
 );
