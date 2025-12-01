1ebbcb5e393c902b939cd7a40d1fb63722b6bc1e
zentoo31 <zentoo31@gmail.com>
2025-11-30 20:19:56 -0500

    feat: add optional description field to Product type

--- src/lib/Product.ts
+++ src/lib/Product.ts
@@ -1,6 +1,7 @@
 export type Product = {
   id: string;          
   created_at?: string;    
+  description?: string;
   name: string;
   price: number;      
   type: 'food' | 'toys' | 'accessories' | 'medicine' | 'other';
